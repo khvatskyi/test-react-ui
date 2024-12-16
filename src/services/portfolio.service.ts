1bb4c5a640c5b01c48b8dcdfd474ac84ada7c79f
Oleksandr Slobodian <khvatskyicode@gmail.com>
2024-12-16 14:43:49 +0100

    MOCK logic for services

--- src/services/portfolio.service.ts
+++ src/services/portfolio.service.ts
@@ -3,7 +3,17 @@ import { IPortfolioDetails } from '../pages/portfolioDetails/portfolioDetails.mo
 
 const BASE_PATH = process.env.REACT_APP_API_ROOT + '/api/portfolio';
 
+const MOCK_DATA: IPortfolio[] = [
+  { id: '1', name: 'Pesho' },
+  { id: '2', name: 'Gosho' },
+  { id: '3', name: 'Stamat' },
+  { id: '4', name: 'Test' }
+]
+
 export function getPortfolios(): Promise<IPortfolio[]> {
+
+  // MOCK data
+  return Promise.resolve(MOCK_DATA);
 
   const response = fetch(BASE_PATH, {
     method: 'GET'
@@ -14,6 +24,18 @@ export function getPortfolios(): Promise<IPortfolio[]> {
 
 export function getPortfolio(id: string): Promise<IPortfolioDetails> {
 
+  // MOCK data
+  return Promise.resolve({
+    ...MOCK_DATA.find(x => x.id === id),
+    description: 'test',
+    industry: 'Insurance',
+    goalsOrObjectives: 'test',
+    businessCapabilities: 'test',
+    industryStandards: 'test',
+    keyPartners: 'test',
+    keySuppliers: 'test'
+  } as IPortfolioDetails)
+
   const path = BASE_PATH + `/${id}`;
   const response = fetch(path, { method: 'GET' });
 
@@ -21,6 +43,20 @@ export function getPortfolio(id: string): Promise<IPortfolioDetails> {
 }
 
 export function savePortfolio(portfolio: IPortfolioDetails): Promise<IPortfolioDetails> {
+
+  // MOCK
+  let result = structuredClone(portfolio);
+
+  if (result.id) {
+    const element = MOCK_DATA.find(x => x.id === portfolio.id)!;
+    element.name = portfolio.name;
+  } else {
+    result.id = crypto.randomUUID();
+    MOCK_DATA.push({ id: result.id, name: portfolio.name });
+  }
+
+  return Promise.resolve(result);
+  //END of MOCK
 
   const response = fetch(BASE_PATH, {
     method: 'POST',
