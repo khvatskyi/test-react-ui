8a3fb54be64f9671b63c244a60aca75720fbf3d3
Oleksandr Slobodian <khvatskyicode@gmail.com>
2025-01-14 08:18:55 +0100

    selector: added sync with form

--- src/pages/PortfolioStages/components/PortfolioStagesTopBar/PortfolioStagesTopBar.tsx
+++ src/pages/PortfolioStages/components/PortfolioStagesTopBar/PortfolioStagesTopBar.tsx
@@ -1,11 +1,14 @@
-import { useState } from 'react';
+import { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+
 import { Button, FlexRow, FlexCell, FlexSpacer, Panel, PickerInput } from '@epam/uui';
 import { useArrayDataSource } from '@epam/uui-core';
 
-import { useAppSelector } from '../../../../hooks';
-import { selectPortfolios } from '../../../../store/data.slice';
+import { useAppDispatch, useAppSelector } from '../../../../hooks';
+import { loadPortfolio, loadPortfolios, selectPortfolioDetails, selectPortfolios } from '../../../../store/data.slice';
 
 import css from './PortfolioStagesTopBar.module.scss';
+import { IPortfolio } from '../../../../typings/models/portfolio.models';
 
 export interface IPortfolioStagesTopBarProps {
   title: string;
@@ -13,21 +16,31 @@ export interface IPortfolioStagesTopBarProps {
 }
 
 export default function PortfolioStagesTopBar({ title, onUpdateClick}: IPortfolioStagesTopBarProps) {
-  const [value, onValueChange] = useState<number[]>();
-
+  const dispatch = useAppDispatch();
   const portfolios = useAppSelector(selectPortfolios);
-  const items = portfolios ? portfolios.map((x) => ({ id: x.id, name: x.name })) : [{id: '', name: ''}];
-  const portfoliosDataSource = useArrayDataSource({ items: items }, []); 
-  
-  return (
+  const selectedPortfolio = useAppSelector(selectPortfolioDetails);
+  const history = useHistory();
+
+  useEffect(() => {
+    dispatch(loadPortfolios());
+  }, [dispatch]);
+
+  const portfoliosDataSource = useArrayDataSource<IPortfolio, string, unknown>({ items: portfolios }, []); 
+
+  const handlePortfolioChange = (id: string) => {
+    dispatch(loadPortfolio(id));
+    history.push(`${id}`);
+  };
+
+  return selectedPortfolio && (
     <Panel cx={css.buttonPanel}>
       <FlexRow columnGap='24' cx={css.buttonPanel}>
         <h2>{title}</h2>
         <FlexCell width='auto'>
           <PickerInput
             dataSource={ portfoliosDataSource }
-            value={ value }
-            onValueChange={ onValueChange }
+            value={ selectedPortfolio.id }
+            onValueChange={ handlePortfolioChange }
             selectionMode='single'
             getName={(item) => item.name}
             valueType='id'
