79c4938f8d9e148ec362a370b559b2263816baaf
Vasyl Prodanchuk <Vasyl_Prodanchuk@epam.com>
2025-01-16 10:42:37 +0100

    PortfolioPicker commented code removed

--- src/components/PortfolioPicker/PortfolioPicker.tsx
+++ src/components/PortfolioPicker/PortfolioPicker.tsx
@@ -1,11 +1,9 @@
 import { useEffect } from 'react';
-// import { useHistory } from 'react-router-dom';
-
 import { PickerInput } from '@epam/uui';
 import { useArrayDataSource } from '@epam/uui-core';
 
 import { useAppDispatch, useAppSelector } from '../../hooks';
-import { /*loadPortfolio, */ loadPortfolios, selectPortfolios } from '../../store/data.slice';
+import { loadPortfolios, selectPortfolios } from '../../store/data.slice';
 import { IPortfolio, IPortfolioDetails } from '../../typings/models/portfolio.models';
 
 export interface IPortfolioPickerProps {
@@ -16,18 +14,12 @@ export interface IPortfolioPickerProps {
 export default function PortfolioPicker({ portfolio, onPortfolioChange}: IPortfolioPickerProps) {
   const dispatch = useAppDispatch();
   const portfolios = useAppSelector(selectPortfolios);
-  // const history = useHistory();
 
   useEffect(() => {
     dispatch(loadPortfolios());
   }, [dispatch]);
 
   const portfoliosDataSource = useArrayDataSource<IPortfolio, string, unknown>({ items: portfolios }, []); 
-
-  // const handlePortfolioChange = (id: string) => {
-  //   dispatch(loadPortfolio(id));
-  //   history.push(`${id}`);
-  // };
 
   const portfolioId = portfolio ? portfolio.id : '';
 
