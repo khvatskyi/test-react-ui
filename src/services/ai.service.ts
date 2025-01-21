7f1767d3cb20e8b4a96d35731a7f591e35cff641
Oleksandr Slobodian <khvatskyicode@gmail.com>
2025-01-21 12:42:46 +0100

    commented MOCKs

--- src/services/ai.service.ts
+++ src/services/ai.service.ts
@@ -1,4 +1,4 @@
-import { delay } from '@epam/uui-test-utils';
+// import { delay } from '@epam/uui-test-utils';
 import { IAiMessage, IAiResponse, IAiClientDefinitionFillRequest, IAiClientDefinitionFillResponse} from '../typings/models/ai.models';
 import { IClientDefinitionInfo, IClientProfileInfo } from '../typings/models/client-info.models';
 import { fetchWithAuth } from '../utilities/fetch-with-auth.utility';
@@ -33,7 +33,7 @@ export async function sendClientDefinitionFillMessage(message: IAiClientDefiniti
   //     ]
   // }
   // return of(response).pipe(delay(500));
-  
+
   const path = process.env.REACT_APP_API_ROOT + '/assistant/client-definition/fill';
   const body = {
     name: message.name ? message.name : ''
@@ -48,29 +48,29 @@ export async function sendClientDefinitionFillMessage(message: IAiClientDefiniti
 
 }
 
-let PROFILE_DATA: IClientProfileInfo = {
-  name: 'Travelers',
-  description: 'Travelers Insurance is one of the largest and most established insurance companies in the United States, providing a wide array of property and casualty insurance products and services to businesses, organizations, and individuals. Founded in 1864, Travelers has built a reputation for financial strength, stability, and handling claims efficiently and fairly. The company focuses on innovation and comprehensive risk management to cater to the changing needs of its clients.',
-  industry: '13',
-  size: 'Large',
-  coreProducts: '',
-  strengths: ['Financial strength', 'Stability', 'Efficient claims handling'],
-  competitiveEdge: 'Focus on innovation and comprehensive risk management',
-  marketPosition: 'One of the largest and most established insurance companies in the US',
-  headquarters: '',
-  primaryLocations: null,
-  website: 'https://www.travelers.com',
-  linkedIn: 'https://linkedin.com/company/travelers',
-  twitter: 'https://twitter.com/travelers',
-  facebook: 'https://www.facebook.com/Travelers',
-  otherSocialMedia: null,
-}
+// let PROFILE_DATA: IClientProfileInfo = {
+//   name: 'Travelers',
+//   description: 'Travelers Insurance is one of the largest and most established insurance companies in the United States, providing a wide array of property and casualty insurance products and services to businesses, organizations, and individuals. Founded in 1864, Travelers has built a reputation for financial strength, stability, and handling claims efficiently and fairly. The company focuses on innovation and comprehensive risk management to cater to the changing needs of its clients.',
+//   industry: '13',
+//   size: 'Large',
+//   coreProducts: '',
+//   strengths: ['Financial strength', 'Stability', 'Efficient claims handling'],
+//   competitiveEdge: 'Focus on innovation and comprehensive risk management',
+//   marketPosition: 'One of the largest and most established insurance companies in the US',
+//   headquarters: '',
+//   primaryLocations: null,
+//   website: 'https://www.travelers.com',
+//   linkedIn: 'https://linkedin.com/company/travelers',
+//   twitter: 'https://twitter.com/travelers',
+//   facebook: 'https://www.facebook.com/Travelers',
+//   otherSocialMedia: null,
+// }
 
 export async function saveClientDefinition(clientDefinition: IClientDefinitionInfo): Promise<IClientProfileInfo> {
 
   // MOCK data
-  PROFILE_DATA = {...PROFILE_DATA, ...structuredClone(clientDefinition)};
-  return delay(2000).then(() => Promise.resolve(PROFILE_DATA));
+  // PROFILE_DATA = {...PROFILE_DATA, ...structuredClone(clientDefinition)};
+  // return delay(2000).then(() => Promise.resolve(PROFILE_DATA));
 
   const path = process.env.REACT_APP_API_ROOT + '/assistant/client-profile/fill';
   const response = await fetchWithAuth(path, {
--- src/services/chat.service.ts
+++ src/services/chat.service.ts
@@ -1,59 +1,59 @@
-import { delay } from '@epam/uui-test-utils';
+// import { delay } from '@epam/uui-test-utils';
 import { STATE_CODES } from '../pages/PortfolioStages/components/PortfolioStagesLeftPanel/structure';
 
 import { IStartChatInfo, IInteractiveChatMessage, IInteractiveChatContext, ChatRole, IMessageToAi } from '../typings/models/module.models';
 import { fetchWithAuth } from '../utilities/fetch-with-auth.utility';
 
-const MOCK_DATA = {
-  interactiveChatMessage: {
-    topic: 'test',
-    text: 'test',
-    example: 'test',
-    totalOfQuestions: 5,
-    questionNumber: 1,
-  } as IInteractiveChatMessage,
-  context: {
-    id: 'teslatdsh',
-    name: 'Business insurance quote enablement',
-    description: 'The API Product will enable insurance brokers and agents',
-    history: [
-      {
-        id: '3432y43',
-        createdBy: ChatRole.AI,
-        content: {
-          topic: 'Some Topic',
-          text: 'Some Question',
-          example: 'Some example',
-          totalOfQuestions: 5,
-          questionNumber: 1,
-        } as IInteractiveChatMessage
-      },
-      {
-        id: '323266',
-        createdBy: ChatRole.User,
-        content: {
-          text: 'Insurance brokers and agents require a streamlined process to request quotes from Travelers Insurance quickly and efficiently, reducing the time spent on manual quote requests and improving customer service.'
-        }
-      },
-      {
-        id: '57655fd',
-        createdBy: ChatRole.AI,
-        content: {
-          topic: 'Some Topic 2 ',
-          text: 'Some Question 2',
-          example: 'Some example 2',
-          totalOfQuestions: 5,
-          questionNumber: 2,
-        } as IInteractiveChatMessage
-      }
-    ]
-  } as IInteractiveChatContext
-}
+// const MOCK_DATA = {
+//   interactiveChatMessage: {
+//     topic: 'test',
+//     text: 'test',
+//     example: 'test',
+//     totalOfQuestions: 5,
+//     questionNumber: 1,
+//   } as IInteractiveChatMessage,
+//   context: {
+//     id: 'teslatdsh',
+//     name: 'Business insurance quote enablement',
+//     description: 'The API Product will enable insurance brokers and agents',
+//     history: [
+//       {
+//         id: '3432y43',
+//         createdBy: ChatRole.AI,
+//         content: {
+//           topic: 'Some Topic',
+//           text: 'Some Question',
+//           example: 'Some example',
+//           totalOfQuestions: 5,
+//           questionNumber: 1,
+//         } as IInteractiveChatMessage
+//       },
+//       {
+//         id: '323266',
+//         createdBy: ChatRole.User,
+//         content: {
+//           text: 'Insurance brokers and agents require a streamlined process to request quotes from Travelers Insurance quickly and efficiently, reducing the time spent on manual quote requests and improving customer service.'
+//         }
+//       },
+//       {
+//         id: '57655fd',
+//         createdBy: ChatRole.AI,
+//         content: {
+//           topic: 'Some Topic 2 ',
+//           text: 'Some Question 2',
+//           example: 'Some example 2',
+//           totalOfQuestions: 5,
+//           questionNumber: 2,
+//         } as IInteractiveChatMessage
+//       }
+//     ]
+//   } as IInteractiveChatContext
+// }
 
 export async function createChat(context: IStartChatInfo, stateCode: STATE_CODES): Promise<IInteractiveChatContext> {
 
   // MOCK
-  return Promise.resolve<IInteractiveChatContext>(MOCK_DATA.context);
+  // return Promise.resolve<IInteractiveChatContext>(MOCK_DATA.context);
   //END of MOCK
 
   const path = process.env.REACT_APP_API_ROOT + `/assistant/${stateCode}/start-chat`;
@@ -68,7 +68,7 @@ export async function createChat(context: IStartChatInfo, stateCode: STATE_CODES
 export async function getChatContext(portfolioId: string, stateCode: STATE_CODES): Promise<IInteractiveChatContext> {
 
   // MOCK
-  return Promise.resolve<IInteractiveChatContext>(portfolioId === '2' ? MOCK_DATA.context : null);
+  // return Promise.resolve<IInteractiveChatContext>(portfolioId === '2' ? MOCK_DATA.context : null);
   //END of MOCK
 
   const params = new URLSearchParams({ portfolioId }).toString();
@@ -82,10 +82,10 @@ export async function getChatContext(portfolioId: string, stateCode: STATE_CODES
 export async function sendChatMessage(message: IMessageToAi, stateCode: STATE_CODES): Promise<IInteractiveChatMessage> {
 
   // MOCK
-  return await delay(1000).then(() => Promise.resolve<IInteractiveChatMessage>({
-    ...MOCK_DATA.interactiveChatMessage,
-    questionNumber: message.isLastAnswer ? MOCK_DATA.interactiveChatMessage.totalOfQuestions : MOCK_DATA.interactiveChatMessage.questionNumber
-  }));
+  // return await delay(1000).then(() => Promise.resolve<IInteractiveChatMessage>({
+  //   ...MOCK_DATA.interactiveChatMessage,
+  //   questionNumber: message.isLastAnswer ? MOCK_DATA.interactiveChatMessage.totalOfQuestions : MOCK_DATA.interactiveChatMessage.questionNumber
+  // }));
   //END of MOCK
 
   const path = process.env.REACT_APP_API_ROOT + `/assistant/${stateCode}/chat`;
@@ -108,12 +108,12 @@ export async function sendChatMessage(message: IMessageToAi, stateCode: STATE_CO
 export async function deleteChat(portfolioId: string, stateCode: STATE_CODES): Promise<void> {
 
   // MOCK
-  return await delay(1000).then(() => Promise.resolve());
+  // return await delay(1000).then(() => Promise.resolve());
   //END of MOCK
 
-  // const params = new URLSearchParams({ portfolioId }).toString();
-  // const path = process.env.REACT_APP_API_ROOT + `/chat/${stateCode}?` + params;
-  // const response = await fetchWithAuth(path, { method: 'DELETE' });
+  const params = new URLSearchParams({ portfolioId }).toString();
+  const path = process.env.REACT_APP_API_ROOT + `/chat/${stateCode}?` + params;
+  const response = await fetchWithAuth(path, { method: 'DELETE' });
 
-  // return await response.json();
+  return await response.json();
 }
